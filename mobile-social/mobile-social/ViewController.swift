abb82201d3b841c998685bab888fa592e3844b19
Yi Zheng <yzheng16@studentmail.nait.ca>
2019-11-05 08:59:50 -0700

    init iOS project
    install pods

---
+++ mobile-social/mobile-social/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mobile-social
+//
+//  Created by Yi Zheng on 2019-10-30.
+//  Copyright © 2019 Yi Zheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
