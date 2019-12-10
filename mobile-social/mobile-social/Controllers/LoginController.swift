b0c4722d086c5f92c4476d7ea5d864049fdd8ff2
Yi Zheng <yzheng16@studentmail.nait.ca>
2019-12-09 21:55:04 -0700

    E12-mobile-social

--- mobile-social/mobile-social/Controllers/LoginController.swift
+++ mobile-social/mobile-social/Controllers/LoginController.swift
@@ -16,12 +16,12 @@ class LoginController: LBTAFormController {
     let emailTextField = IndentedTextField(placeholder: "Email", padding: 24, cornerRadius: 25, keyboardType: .emailAddress, backgroundColor: .white, isSecureTextEntry: false)
     let passwordTextField = IndentedTextField(placeholder: "Password", padding: 24, cornerRadius: 25, keyboardType: .default, backgroundColor: .white, isSecureTextEntry: true)
     lazy var loginButton = UIButton(title: "Login", titleColor: .white, font: .boldSystemFont(ofSize: 18), backgroundColor: .black, target: self, action: #selector(handleLogin))
-    let errorLable = UILabel(text: "Your login credentials were incorrect, please try again", font: .systemFont(ofSize: 14), textColor: .red, textAlignment: .center, numberOfLines: 0)
-    lazy var goToRegisterButton = UIButton(title: "Need an account? Go to  register.", titleColor: .black, font: .systemFont(ofSize: 16), target: self, action: #selector(goToRegister))
+    let errorLabel = UILabel(text: "Your login credentials were incorrect, please try again", font: .systemFont(ofSize: 14), textColor: .red, textAlignment: .center, numberOfLines: 0)
+    lazy var goToRegisterButton = UIButton(title: "Need an account? Go to register.", titleColor: .black, font: .systemFont(ofSize: 16), target: self, action: #selector(goToRegister))
     
     @objc fileprivate func goToRegister() {
 //        ------in first page-----
-        let registerController = RegisterController()
+        let registerController = RegisterController(alignment: .center)
         navigationController?.pushViewController(registerController, animated: true)
 //        ------go back in second page----
 //        navigationController?.popViewController(animated: true)
@@ -37,42 +37,42 @@ class LoginController: LBTAFormController {
         guard let email = emailTextField.text else {return}
         guard let password = passwordTextField.text else {return}
         
-        errorLable.isHidden = true
+        errorLabel.isHidden = true
         
         let url = "http://localhost:1337/api/v1/entrance/login"
         let params = ["emailAddress": email, "password": password]
         Alamofire.request(url, method: .put, parameters: params, encoding: URLEncoding())
             .validate(statusCode: 200..<300)
             .responseData { (dataResponse) in
-            hud.dismiss()
-            if let _ = dataResponse.error {
-                self.errorLable.isHidden = false
-                self.errorLable.text = "Your email or password is not correct. Please try again"
-                return
-            }
-            print("Finally sent request to server..")
-            self.dismiss(animated: true)
+                hud.dismiss()
+                if let _ = dataResponse.error {
+                    self.errorLabel.isHidden = false
+                    self.errorLabel.text = "Your email or password is not correct. Please try again"
+                    return
+                }
+                print("Finally sent request to server..")
+                self.dismiss(animated: true)
         }
-    }    
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .init(white: 0.95, alpha: 1)
         
         navigationController?.navigationBar.isHidden = true
-        errorLable.isHidden = true
+        errorLabel.isHidden = true
         
         let formView = UIView()
         formView.stack(
             emailTextField.withHeight(50),
             passwordTextField.withHeight(50),
             loginButton.withHeight(50),
-            errorLable,
+            errorLabel,
             goToRegisterButton,
             spacing:16
         ).withMargins(.init(top: 48, left: 32, bottom: 0, right: 32))
         
-        formContainerStackView.padBottom(-24)
+//        formContainerStackView.padBottom(-24)
         formContainerStackView.addArrangedSubview(formView)
     }
 }
--- mobile-social/mobile-social/Controllers/RegisterController.swift
+++ mobile-social/mobile-social/Controllers/RegisterController.swift
@@ -6,10 +6,71 @@
 //  Copyright © 2019 Yi Zheng. All rights reserved.
 //
 
-import UIKit
+import LBTATools
+import Alamofire
+import JGProgressHUD
 
-class RegisterController: UIViewController {
+class RegisterController: LBTAFormController {
+    
+    //MARK: UI Elements
+    //indentedTextField is from LBTATools
+    let fullNameTextField = IndentedTextField(placeholder: "Full Name", padding: 24, cornerRadius: 25, keyboardType: .default, backgroundColor: .white, isSecureTextEntry: false)
+    let emailTextField = IndentedTextField(placeholder: "Email", padding: 24, cornerRadius: 25, keyboardType: .emailAddress, backgroundColor: .white, isSecureTextEntry: false)
+    let passwordTextField = IndentedTextField(placeholder: "Password", padding: 24, cornerRadius: 25, keyboardType: .default, backgroundColor: .white, isSecureTextEntry: true)
+    lazy var signUpButton = UIButton(title: "Sign Up", titleColor: .white, font: .boldSystemFont(ofSize: 18), backgroundColor: .black, target: self, action: #selector(handleSignup))
+    
+    let errorLabel = UILabel(text: "Something went wrong during sign up, please try again later", font: .systemFont(ofSize: 14), textColor: .red, textAlignment: .center, numberOfLines: 0)
+    lazy var goBackButton = UIButton(title: "Go back to login", titleColor: .black, font: .systemFont(ofSize: 16), backgroundColor: .white, target: self, action: #selector(goToRegister))
+    
+    @objc fileprivate func goToRegister(){
+        navigationController?.popViewController(animated: true)
+    }
+    
+    @objc fileprivate func handleSignup() {
+        let hud = JGProgressHUD(style: .dark)
+        hud.textLabel.text = "Registering"
+        hud.show(in: view)
+        
+        guard let fullName = fullNameTextField.text else {return}
+        guard let email = emailTextField.text else {return}
+        guard let password = passwordTextField.text else {return}
+        
+        let url = "http://localhost:1337/api/v1/entrance/signup"
+        let params = ["fullName": fullName, "emailAddress": email, "password": password]
+        Alamofire.request(url, method: .post, parameters: params)
+            .validate(statusCode: 200..<300)
+            .responseData { (dataResponse) in
+                hud.dismiss()
+                if let err = dataResponse.error {
+                    self.errorLabel.isHidden = false
+                    print("Failed to sign up:", err)
+                    return
+                }
+                print("Successfully signed up")
+                self.dismiss(animated: true)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        errorLabel.isHidden = true
+        emailTextField.autocapitalizationType = .none
+        [fullNameTextField, emailTextField, passwordTextField].forEach{$0.backgroundColor = .white}
+        signUpButton.layer.cornerRadius = 25
+        
+        let formView = UIView()
+        formView.stack(
+            UIView().withHeight(12),
+            fullNameTextField.withHeight(50),
+            emailTextField.withHeight(50),
+            passwordTextField.withHeight(50),
+            errorLabel,
+            signUpButton.withHeight(50),
+            goBackButton,
+            UIView().withHeight(80),
+            spacing: 16).withMargins(.init(top: 48, left: 32, bottom: 0, right: 32))
+        
+        formContainerStackView.addArrangedSubview(formView)
     }
 }
