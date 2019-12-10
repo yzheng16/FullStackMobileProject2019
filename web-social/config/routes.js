7dc9c8a3b28a0d4771ddcead85505bc2699151f1
Yi Zheng <yzheng16@studentmail.nait.ca>
2019-12-09 21:56:03 -0700

    E12-web-social
    fixed status code 403

--- web-social/config/routes.js
+++ web-social/config/routes.js
@@ -58,7 +58,7 @@ module.exports.routes = {
   'PUT   /api/v1/account/update-billing-card':        { action: 'account/update-billing-card' },
   'PUT   /api/v1/entrance/login':                        { action: 'entrance/login', csrf: false },
   // 'POST   /api/v1/entrance/login':                        { action: 'entrance/login', csrf: false },
-  'POST  /api/v1/entrance/signup':                       { action: 'entrance/signup' },
+  'POST  /api/v1/entrance/signup':                       { action: 'entrance/signup', csrf: false },
   'POST  /api/v1/entrance/send-password-recovery-email': { action: 'entrance/send-password-recovery-email' },
   'POST  /api/v1/entrance/update-password-and-login':    { action: 'entrance/update-password-and-login' },
   'POST  /api/v1/deliver-contact-form-message':          { action: 'deliver-contact-form-message' },
