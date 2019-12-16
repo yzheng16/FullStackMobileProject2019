01f2eb005b7413e39da0cdef4cf3962392de2fa0
Yi Zheng <yzheng16@studentmail.nait.ca>
2019-12-15 20:41:23 -0700

    E13

--- web-social/api/controllers/post/create.js
+++ web-social/api/controllers/post/create.js
@@ -1,8 +1,28 @@
 module.exports = async function(req, res) {
     const postBody = req.body.postBody;
     console.info('creating a new post ' + postBody);
-    const userId = req.session.userId;
-    await Post.create({text: postBody, user: userId});
-    // res.send(record);
-    res.redirect('/post')
+
+    //imagefile matchs the name in the input DOM name
+    const file = req.file('imagefile')
+
+    // I want to upload my file above
+    file.upload({
+        adapter: require('skipper-s3'),
+        key: 'S3 Key',
+        secret: 'S3 Secret',
+        bucket: 'Bucket Name'
+    }, function (err, filesUploaded) {
+
+        if (err) return res.serverError(err);
+
+        return res.ok({
+            files: filesUploaded,
+            textParams: req.allParams()
+        });
+    });
+
+    // const userId = req.session.userId;
+    // await Post.create({text: postBody, user: userId});
+    // // res.send(record);
+    // res.redirect('/post')
 }
